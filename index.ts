1e3846173f303788d05b157fb50e1250a8f187c3
natalia <53310862+natmarek@users.noreply.github.com>
2022-07-17 17:09:06 +0100

    First commit

---
+++ index.ts
@@ -0,0 +1,16 @@
+import * as pulumi from "@pulumi/pulumi";
+import * as aws from "@pulumi/aws";
+import * as awsx from "@pulumi/awsx";
+
+// Create an AWS resource (S3 Bucket)
+const bucket = new aws.s3.Bucket("my-bucket");
+
+// Export the name of the bucket
+export const bucketName = bucket.id;
+
+const bucketObject = new aws.s3.BucketObject("index.html", {
+    bucket: bucket,
+    source: new pulumi.asset.FileAsset("index.html")
+});
+
+
